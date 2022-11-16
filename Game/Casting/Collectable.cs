5142050be59c56ff74f90ae5834df2e895778a4c
Orange4me <brandonalder@byui.edu>
2022-11-16 09:32:34 -0700

    setup

---
+++ Game/Casting/Collectable.cs
@@ -0,0 +1,23 @@
+namespace Greed.Game.Casting
+{
+    public class Collectable : Actor
+    {
+
+    private int _points;
+
+    public Collectable()
+    {
+        
+    }
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+    public void SetPoints(int points)
+    {
+        _points = points;
+    }
+
+    }
+}
